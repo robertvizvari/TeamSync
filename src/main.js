332768e319caa8f4d14bccb8f75357619735fb16
Robert Vizvari <vizvarirobko9@gmail.com>
2024-09-26 16:40:40 +0200

    Setup

--- src/main.js
+++ src/main.js
@@ -4,22 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-
-// PrimeVue components import
-import Button from "primevue/button"
-
-
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-      preset: Aura
-  }
-});
-
-app.component('Button', Button)
 
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
-  <h1 class="p-4 text-2xl">Home</h1>
-  <Button label="button" />
-  <i class="pi pi-check"></i>
-
+  <div class="bg-background">
+    <Header />
+    <section>
+      <div class="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 sm:px-16 lg:text-xl xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <div class="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
+          <a href="#" class="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-white focus:ring-4">
+            Learn more
+            <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+          <a href="#" class="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <svg class="-ml-1 mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+            Watch video
+          </a>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script setup>
+import Header from '../components/Header.vue'
+</script>
